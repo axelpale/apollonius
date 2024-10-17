86692d8d3a96abff3c2f8dfc4fcde7e94f1abd6b
Akseli Palen <akseli.palen@gmail.com>
2024-10-17 16:08:22 +0300

    build umd bundle and consume it in the demo

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,21 @@
+import terser from '@rollup/plugin-terser'
+// NOTE Experimental JSON import
+import { default as pjson } from './package.json' with { type: 'json' }
+
 export default {
   input: 'index.js',
   output: [
     {
-      file: 'index.cjs',
+      file: 'dist/index.cjs',
       format: 'cjs',
       exports: 'named'
+    },
+    {
+      file: `dist/${pjson.name}-${pjson.version}.min.js`,
+      format: 'umd',
+      name: 'apollonius',
+      exports: 'named',
+      plugins: [terser()]
     }
   ]
 }
