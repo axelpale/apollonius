aebf50bccd43e5c8851572cd80faee2eb426ebad
Akseli Palen <akseli.palen@gmail.com>
2024-11-08 04:53:07 +0200

    use ts-expect-error to run tests on type declarations

--- test/types.ts
+++ test/types.ts
@@ -1,12 +1,13 @@
 // Test TypeScript type declarations compatibilty.
+//
 import { solve, options } from '../index.js'
 
 const c1 = { x: 1, y: 2, r: 1 }
 const c2 = { x: 3, y: 2, r: 1 }
 const c3 = 'bad'
 
+// @ts-expect-error
 const c = solve(c1, c2, c3)
 
-// options.epsilon = 'bad'
-
-// console.log(c, options.epsilon)
+// @ts-expect-error
+options.epsilon = 'bad'
